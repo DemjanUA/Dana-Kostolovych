c5473e3eaabde7e924bd85dea3f6a50817845158
Dmytro Uchkin <dmytro.uchkin@gmail.com>
2018-12-06 13:05:36 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var ScrollControler = function(node) {
+    this.constructor = function () {
+        console.log('build ---------->', this);
+
+        if (document.querySelector('[data-component="pickButtonsBar"]') && document.querySelector('.pick-buttons-bar')) {
+            this.pickButtonsBar = new PickButtonsBar().constructor();
+
+            window.addEventListener('scroll', () => {
+                this.pickButtonsBar.pickButtonChange();
+            });
+        }
+
+
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset === 0) {
+                lock();
+            }  else if (!this.unlockFlag) {
+                unlock();
+            }
+        });
+        return this;
+    };
+
+    this.node = node;
+    this.unlockFlag = 0;
+
+    var lock = (function() {
+        this.node.dataset['locker'] = 'lock';
+        this.unlockFlag = 0;
+    }).bind(this);
+
+    var unlock = (function() {
+        this.node.dataset['locker'] = 'unlock';
+        this.unlockFlag = 1;
+    }).bind(this);
+}
+
+var LogoScroll = function(node) {
+    this.constructor = function () {
+        console.log('build ---------->', this);
+        this.logoNode.addEventListener('click', this.scrollEvent, false);
+        return this;
+    };
+
+    this.logoNode = node;
+
+    this.scrollEvent = function(event) {
+        var timer = setInterval(function() {
+            var bufferNumber = window.pageYOffset - 100;
+            if (bufferNumber <= 0) {
+                window.scrollTo(0, 0);
+                clearInterval(timer);
+            } else {
+                window.scrollTo(0, bufferNumber - 100);
+            }
+        }, 5);
+
+        event.preventDefault();
+    };
+} 
+
+
+var PickButtonsBar = function() {
+    this.constructor = function () {
+        console.log('build ---------->', this);
+
+
+        let itemPack = document.createDocumentFragment();
+        this.itemList.forEach(e => {
+            let item = e.cloneNode(2);
+            itemPack.appendChild(item);
+        });
+        this.bar.appendChild(itemPack);
+
+        this.buttonList = Array.prototype.map.call(this.bar.children, e => e);
+        this.activeButton = this.buttonList[0];
+        this.activeButton.classList.add('active');
+
+        this.bar.addEventListener('click', function(e) {
+            if (e.target.nodeName.toLowerCase() === 'a') {
+                //console.log(e.target);
+            }
+        });
+
+        return this;
+    };
+    this.plan = document.querySelector('[data-component="pickButtonsBar"]');
+    this.bar = document.querySelector('.pick-buttons-bar');
+    this.itemList = Array.prototype.map.call(this.plan.children, e => e);
+    this.sectionList = Array.prototype.map.call(document.querySelectorAll('section'), e => e);
+}
+
+PickButtonsBar.prototype.pickButtonChange = function() {
+    this.sectionList.some((e, index) => {
+        let positionOfStart = e.getBoundingClientRect().top + window.pageYOffset - 65;
+        let positionOfEnd = positionOfStart + e.clientHeight  - 65;
+
+        if (window.pageYOffset >= positionOfStart && window.pageYOffset <= positionOfEnd) {
+            this.activeButton.classList.remove('active');
+            this.activeButton = this.buttonList[index];
+            this.activeButton.classList.add('active');
+            return true;
+        }
+    });
+};
+
+new ScrollControler(document.querySelector('header')).constructor();
+document.querySelector('a.logo') && new LogoScroll(document.querySelector('a.logo')).constructor();
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/statistic.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+};
